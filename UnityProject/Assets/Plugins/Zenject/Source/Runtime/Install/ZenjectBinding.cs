5f2633a143c93fa9c201b7f0095609eccb3dc107
Páll Arinbjarnar <47219668+PallArinbj@users.noreply.github.com>
2024-03-06 08:58:53 +0000

    Migrated a previously done Aldin fix to ZenjectBinding.cs

--- UnityProject/Assets/Plugins/Zenject/Source/Runtime/Install/ZenjectBinding.cs
+++ UnityProject/Assets/Plugins/Zenject/Source/Runtime/Install/ZenjectBinding.cs
@@ -9,7 +9,7 @@ namespace Zenject
     {
         [Tooltip("The component to add to the Zenject container")]
         [SerializeField]
-        Component[] _components = null;
+        Object[] _components = null;
 
         [Tooltip("Note: This value is optional and can be ignored in most cases.  This can be useful to differentiate multiple bindings of the same type.  For example, if you have multiple cameras in your scene, you can 'name' them by giving each one a different identifier.  For your main camera you might call it 'Main' then any class can refer to it by using an attribute like [Inject(Id = 'Main')]")]
         [SerializeField]
@@ -39,7 +39,7 @@ namespace Zenject
             set { _context = value; }
         }
 
-        public Component[] Components
+        public Object[] Components
         {
             get { return _components; }
         }
